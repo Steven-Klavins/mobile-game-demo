5122a91d1c342295eb643116d7146ea202d5aed7
Elon <56255988+Br3adFruit@users.noreply.github.com>
2021-02-08 00:57:51 +0000

    fiddling with background
    - Imported small assets from store(temporary)
    - Created 2D background
    - Created script to try to move background

---
+++ MobileGame(Steve)/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+     BoxCollider2D Collider;
+     Rigidbody2D rb;
+
+    float height;
+    float scrollSpeed = -2f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Collider = GetComponent<BoxCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
+
+        height = Collider.size.y;
+        Collider.enabled = false;
+        rb.velocity = new Vector2(0, scrollSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -height)
+        {
+            Vector2 resetPosition = new Vector2(0, height * 2);
+            transform.position = (Vector2)transform.position + resetPosition;
+
+        } 
+    }
+}
