0a20e2748f6b2bfe935cbce936c3b15ebf76fa24
Elon <56255988+Br3adFruit@users.noreply.github.com>
2021-02-11 19:49:09 +0000

    Working BG scroll / Camera follow
    -Made the background follow the camera & player
    
    - Deleted unnecessary animations/scripts

--- MobileGame(Steve)/Assets/Scripts/BasicMovement.cs
+++ MobileGame(Steve)/Assets/Scripts/BasicMovement.cs
@@ -6,12 +6,24 @@ public class BasicMovement : MonoBehaviour
 {
 
     public float jHeight = 6;
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+       
+    }
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W))
+        if(Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.up * jHeight * Time.deltaTime);
+            rb.gravityScale = 0;
+            rb.transform.Translate(Vector3.up * jHeight * Time.deltaTime);
+        }
+        else
+        {
+            rb.gravityScale = 1;
         }
     }
 }
--- MobileGame(Steve)/Assets/Scripts/CameraControl.cs
+++ MobileGame(Steve)/Assets/Scripts/CameraControl.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     public Transform target;
-    
+    [SerializeField]
+    private Transform centerBackground;
 
     // Start is called before the first frame update
     void Start()
@@ -20,8 +21,16 @@ public class CameraControl : MonoBehaviour
         Vector3 targetPos = new Vector3(target.position.x, target.position.y, target.position.z);
         transform.position = Vector3.Lerp(transform.position, targetPos, 0.2f);
 
-  
 
+        //Background Scrolling
+        if (transform.position.y >= centerBackground.position.y + 10)
+        {
+            centerBackground.position = new Vector2(centerBackground.position.x, transform.position.y + 10);
+        }
+        else if (transform.position.y <= centerBackground.position.y - 10)
+        {
+            centerBackground.position = new Vector2(centerBackground.position.x, transform.position.y - 10);
+        }
     }
 
 }
